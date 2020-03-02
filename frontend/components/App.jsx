5ac4c098c514f64bee6f5528f2e98824c365f211
jwchau <john.win.chau@gmail.com>
2020-03-01 18:19:40 -0800

    setup part 2

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,19 +4,17 @@ import GreetingContainer from './greetings/greeting_container';
 import LFC from './sessions/LoginFormContainer';
 import SFC from './sessions/SignupFormContainer';
 import { AuthRoute } from '../util/route_util';
-import BenchIndexContainer from '../components/benches/bench_index_container';
 
 
 const App = () => (
   <div>
-    <h1>Bench BnB</h1>
+    <h1>IMDB Clone</h1>
     <GreetingContainer />
     <Switch>
       <AuthRoute path='/signup' component={SFC} />
       <AuthRoute path='/login' component={LFC} />
       <Route path='/' render={() => <div className='splash'>Splashin'</div>}/>
     </Switch>
-    <Route exact path="/" component={BenchIndexContainer} />
   </div>
 );
 
--- frontend/reducers/session_error_reducer.js
+++ frontend/reducers/session_error_reducer.js
@@ -1,4 +1,5 @@
-import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER} from '../util/session_api_util'
+// import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER} from '../util/session_api_util'
+import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 export default (state = [], action) => {
     Object.freeze(state)
