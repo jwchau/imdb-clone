ec8bd55efb2c338de6f1486c7aeec9b82ecca432
jwchau <john.win.chau@gmail.com>
2020-03-03 14:21:17 -0800

    complete footer

--- frontend/components/frontpage/footer_container.jsx
+++ frontend/components/frontpage/footer_container.jsx
@@ -7,9 +7,40 @@ class Footer extends React.Component {
     }
 
     render() {
-
         return(
-            <div>footer</div>
+            <footer id='footer'>
+                <div className='social-media'>
+                    <div className='icon'><img src="assets/fb_logo.svg"/></div>
+                    <div className='icon'><img src="assets/insta_logo.svg" /></div>
+                    <div className='icon'><img src="assets/twitch_logo.svg" /></div>
+                    <div className='icon'><img src="assets/twitter_logo.svg" /></div>
+                    <div className='icon'><img src="assets/youtube_logo.svg" /></div>
+                </div>
+
+                <div className='misc'>
+                    <div class='misc-link'>Get the IMDb App</div>
+                    <div class='misc-link'>Help</div>
+                    <div class='misc-link'>Site Index</div>
+                    <div class='misc-link'>IMDbPro</div>
+                    <div class='misc-link'>IMDb TV</div>
+                    <div class='misc-link'>Box Office Mojo</div>
+                    <div class='misc-link'>Press Room</div>
+                    <div class='misc-link'>Advertising</div>
+                </div>
+
+                <div className='misc'>
+                    <div class='misc-link'>Jobs</div>
+                    <div class='misc-link'>Conditions of Use</div>
+                    <div class='misc-link'>Privacy Policy</div>
+                    <div class='misc-link'>Internet-Based Ads</div>
+                </div>
+       
+
+                
+
+                <div className='company-logo'> ∞ </div>
+                <div className='founded'>© 1900-2077 by John, Inc.</div>
+            </footer>
         );
     }
 }
