84be3a7a6a8b217bb48ad143fc0cfcd720037fa7
jwchau <john.win.chau@gmail.com>
2020-04-10 16:35:02 -0700

    overflow and videos implementation

--- frontend/components/movies/movie/movie_videos.jsx
+++ frontend/components/movies/movie/movie_videos.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const MovieVideos = (props) => {
+const extractVideos = arr => {
+  return arr.map(ele => (
+    <li>
+      <iframe
+        src={`https://www.youtube.com/embed/${ele.key}`} frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen>
+      </iframe>
+    </li>
+  ));
+};
 
+const MovieVideos = (props) => {
+  if (props.videos.length < 1) return null;
   return (
     <div className='videos'>
-      i am movie videos
+      <h3>Videos</h3>
+      <ul className='video-list'>
+        {extractVideos(props.videos)}
+      </ul>
     </div>
   );
 
