6bfb3375a1ae078cbca07bbd38dcf2a6b673ce04
jwchau <john.win.chau@gmail.com>
2020-04-14 16:44:05 -0700

    person show page, get details

--- frontend/components/people/person_show_container.jsx
+++ frontend/components/people/person_show_container.jsx
@@ -5,6 +5,10 @@ import {
   getDetailsPerson,
 } from '../../util/movies_api_util';
 
+import {
+  sourcePicture,
+} from '../../util/util';
+
 
 import Loading from '../loading/loading';
 
@@ -35,22 +39,37 @@ class PersonShow extends Component {
   }
 
   render() {
-    if (!this.state.details.adult) return <Loading />;
+    if (!this.state.details.id) return <Loading />;
+    const {
+      biography, birthday, deathday,
+      homepage, known_for_department:kfd, name,
+      place_of_birth:pob, profile_path:pUrl,
+    } = this.state.details;
     return (
       <div className='person-show flex-col start-center'>
-         <span></span>
+        <h3>{name}</h3>
+        <img src={sourcePicture(pUrl)} alt=""/>
+        {(valid(homepage)) ? <span>Homepage: <a href={homepage}>{homepage}</a></span> : null}
+        {(valid(pob)) ? <span>Place of Birth: <span>{pob}</span></span> : null}
+        {(valid(birthday)) ? <span>Birthday: <span>{birthday}</span></span> : null}
+        {(valid(deathday)) ? <span>Deathday: <span>{deathday}</span></span> : null}
+        {(valid(kfd)) ? <span>Known for: <span>{kfd}</span></span> : null}
+        {(valid(biography)) ? <span>Biography: <span>{biography}</span></span> : null}
       </div>
-    )
+    );
   }
+}
+
+const valid = (str) => {
+  if (str !== null && str !== "" && str !== 0 && str.length !== 0) return true;
+  return false;
 }
 
 const MSTP = (state, ownProps) => ({
   id: parseInt(ownProps.match.params.id),
-
 });
 
 const MDTP = (dispatch, ownProps) => ({
-
 });
 
 export default connect(
