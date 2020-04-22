90b1b0d178608677fce3326dc10016be2cf72c1c
john chau <john.win.chau@gmail.com>
2020-04-21 18:46:30 -0700

    remove console log for key

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,4 @@
 if (process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV);
-  console.log(process.env.TMDB_KEY);
-  console.log(process.env.LANG);
   module.exports = require('./keys_prod');
 } else {
   module.exports = require('./keys_dev');
