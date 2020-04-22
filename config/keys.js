055536d0253572e3265addbc0118f0ee5ad7460f
jwchau <john.win.chau@gmail.com>
2020-04-21 17:49:47 -0700

    testing logs

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  console.log('loaded');
+  console.log(process.env);
   module.exports = require('./keys_prod');
 } else {
   module.exports = require('./keys_dev');
